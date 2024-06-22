54e37501f9511f7fe730b412482d567f9b4041ca
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-06-22 14:30:42 -0500

    improvements

--- projecteuler/bin/make_primes.sh
+++ projecteuler/bin/make_primes.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Factoring and initializing Prime Numbers up to 700_000_000 ... this may take up to 10 minutes"
+echo "Factoring and initializing Prime Numbers up to 700_000_000 ... this may take up to 20 minutes"
 
 python _make_and_print_primes.py --max_num 700000000 --file_name primes_to_700_mil &
 
