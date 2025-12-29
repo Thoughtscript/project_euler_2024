88f7d72248e57df4afdea373f98ea20893e9b3cc
Adam Gerard <adam.intae.gerard@protonmail.com>
2025-12-29 13:53:41 -0600

    recapitulate 836 solution

--- projecteuler/solutions/project_euler_836.py
+++ projecteuler/solutions/project_euler_836.py
@@ -5,9 +5,30 @@ import time
 if __name__ == '__main__':
 
     try:
-        ALGO_BEGIN = time.time()
+
         result_data = initialize(836, "aprilfoolsjoke", 0, 0, [])
-        conclude(result_data, "aprilfoolsjoke", ALGO_BEGIN)
+
+        def solve():
+            ALGO_BEGIN = time.time()
+
+            result = "affine"[0]
+            result += "plane"[0]
+            result += "radically"[0]
+            result += "integral"[0]
+            result += "local"[0]
+            result += "field"[0]
+            result += "open"[0]
+            result += "oriented"[0]
+            result += "line"[0]
+            result += "section"[0]
+            result += "jacobian"[0]
+            result += "orthogonal"[0]
+            result += "kernel"[0]
+            result += "embedding"[0]
+            
+            conclude(result_data, result, ALGO_BEGIN)
+
+        solve() #aprilfoolsjoke
 
     except Exception as ex:
 
